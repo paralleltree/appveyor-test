5d9496d83068326e5bcfd0db9905d91e28e49d09
Paralleltree <paralleltree@outlook.com>
2016-03-31 22:10:15 +0900

    Add test project

---
+++ AppveyorTest.Tests/AdditionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+using AppveyorTest;
+
+namespace AppveyorTest.Tests
+{
+    public class AdditionTest : MathTest<Addition>
+    {
+        //public override void OperationTest(int a, int b)
+        //{
+        //}
+    }
+
+    public class AdditionTest2 : MathTest<Addition>
+    {
+
+    }
+
+    [TestFixture]
+    public abstract class MathTest<T>
+    {
+        [Test]
+        [TestCase(0, 5)]
+        [TestCase(4, 9)]
+        public void OperationTest(int a, int b)
+        {
+            Assert.AreEqual(a + b, new Addition().Add(a, b));
+        }
+    }
+}
